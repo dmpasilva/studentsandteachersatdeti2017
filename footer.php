229781f84b85f303fe3143508f84380c292fbcf3
David Silva <davidsilva@aluno-7330.wireless.ua.pt>
2017-04-20 19:21:34 +0100

    Added source code link

--- footer.php
+++ footer.php
@@ -7,7 +7,8 @@
             ?>
         </p>
         <br />
-        <p align="center" class="copyright"><b>Desenvolvido por <a href="https://davidsilva.pt/">David Silva</a></b></p>
+        <p align="center" class="copyright"><b>Desenvolvido por <a href="https://davidsilva.pt/">David Silva</a></b> 
+            (<a href="https://github.com/dmpasilva/studentsandteachersatdeti2017">código-fonte</a>)</p>
         <p align="center" class="copyright"><b>Edições anteriores:</b> <a href="/2015">2015</a> | <a href="/2016">2016</a></p>
     </div>
 </footer>
